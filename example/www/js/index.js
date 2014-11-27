12902962ae76e858012b007a7572293a3850c608
Narek <narek@voxofon.com>
2014-11-27 20:09:39 +0400

    Added the basic example project (config.xml and www folder only) from telerik demo app.

---
+++ example/www/js/index.js
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+
+    //cordova plugins add ~/Source/Telerik/Plugins/Bit6/Plugin/ --variable API_KEY=308x-3bnqo
+
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('messageReceived', this.onMessageReceived, false);
+    },
+
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        $("#signup").click(function(){
+            bit6.register($("#username").val(), "111", function(success){
+                alert(JSON.stringify(success));
+            }
+            , function(error){
+              alert(JSON.stringify(error));
+            });
+        });
+
+        $("#login").click(function(){
+           bit6.logout();
+
+           bit6.login($("#username").val(), "111", function(success){
+               console.log(JSON.stringify(success));
+           }, function(error){
+             alert(JSON.stringify(error));
+           });
+        });
+
+        $("#sendMessage").click(function(){
+            bit6.sendPushMessage($("#message").val(), "telerik", function(success){
+              console.log(JSON.stringify(success));
+            }, function(error){
+              alert(JSON.stringify(error));
+            });
+        });
+
+    },
+    onMessageReceived : function(e){
+      $("#incoming").html("");
+
+      for(var index = 0; index < e.messages.length;index++){
+            var div = $("<div/>");
+            var displayName = e.messages[index].other.displayName;
+            if (!e.messages[index].incoming){
+              displayName = "Me";
+            }
+
+
+            div.append("<h3>" + displayName + "</h3>");
+            div.append("<p>" + e.messages[index].content + "</p>");
+            $("#incoming").append($(div).html());
+
+        }
+    }
+};
